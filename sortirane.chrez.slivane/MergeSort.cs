a3d8bd10b4b598af0a4ac140844fc09c7ad0a309
sweetblasphemy <nik2006@abv.bg>
2023-06-11 12:51:35 +0300

    Add project files.

---
+++ sortirane.chrez.slivane/MergeSort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortirane.chrez.slivane
+{
+    public class MergeSort
+    {
+
+        public static void Mergesort(int[] arr)
+        {
+            if (arr.Length <= 1)
+                return;
+
+            int mid = arr.Length / 2;
+            int[] leftHalf = new int[mid];
+            int[] rightHalf = new int[arr.Length - mid];
+
+            Array.Copy(arr, 0, leftHalf, 0, mid);
+            Array.Copy(arr, mid, rightHalf, 0, arr.Length - mid);
+
+            Mergesort(leftHalf);
+            Mergesort(rightHalf);
+
+            Merge(arr, leftHalf, rightHalf);
+        }
+
+        public static void Merge(int[] arr, int[] left, int[] right)
+        {
+            int i = 0, j = 0, k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < left.Length)
+            {
+                arr[k] = left[i];
+                i++;
+                k++;
+            }
+
+            while (j < right.Length)
+            {
+                arr[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+
+    }
+
+}
---
+++ sortirane.chrez.slivane/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic;
+using System.Security.Cryptography;
+
+namespace sortirane.chrez.slivane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                int[] array = { 7, 2, 1, 6, 8, 5, 3, 4 };
+
+                Console.WriteLine("Unsorted array:");
+                foreach (int element in array)
+                {
+                    Console.Write(element + " ");
+                }
+
+                Mergesort(array);
+
+                Console.WriteLine("\nSorted array:");
+                foreach (int element in array)
+                {
+                    Console.Write(element + " ");
+                }
+
+                Console.ReadLine();
+            
+        }
+    }
+}
